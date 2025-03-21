9dbac27a3dc9fd8de23a4062502f10fb71850c95
Artur Koltyrin <koltyrinartur@gmail.com>
2025-03-21 07:26:21 +0500

    variables for working with mail have been taken out, authorization buttons have been made on the main page

--- users/urls.py
+++ users/urls.py
@@ -23,7 +23,7 @@ from users.views import RegisterView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
